3aaeeb390e441c58ed08757f9b28fa5890944ac3
EldorPardaevDeveloper <eldorpardayev18@gmail.com>
2024-01-01 18:12:05 +0500

    INFRA: Upload Project

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Telefon
+
+ int oldStr = Convert.ToInt32(Console.ReadLine());
+ 
+ string oldMessage =
+     7 >= oldStr
+        ? "telefon"
+        : "TELEFON";
+
+        System.Console.WriteLine(oldMessage);
+
+
+ //Kompyuter
+ int str = Convert.ToInt32(Console.ReadLine());
+ 
+ string message =
+     8 >= str
+        ? "kompyuter"
+        : "KOMPYUTER";
+
+        System.Console.WriteLine(message);
