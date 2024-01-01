a92c5ddd59cb1c257643cb7dbc22f5221d77a1cd
EldorPardaevDeveloper <eldorpardayev18@gmail.com>
2024-01-01 20:00:22 +0500

    INFRA:Upload Project 2

--- Program.cs
+++ Program.cs
@@ -1,22 +1,26 @@
-﻿
-//Telefon
+﻿//hafta kunlari
+string operation = Console.ReadLine();
 
- int oldStr = Convert.ToInt32(Console.ReadLine());
- 
- string oldMessage =
-     7 >= oldStr
-        ? "telefon"
-        : "TELEFON";
+string dushanba = "monday";
+string seshanba = "tuesday";
+string chorshanba = "wednesday";
+string payshanba = "thursday";
+string juma = "friday";
+string shanba = "saturday";
+string yakshanba = "sunday";
 
-        System.Console.WriteLine(oldMessage);
 
+string result = operation switch 
 
- //Kompyuter
- int str = Convert.ToInt32(Console.ReadLine());
- 
- string message =
-     8 >= str
-        ? "kompyuter"
-        : "KOMPYUTER";
+{
+    "dushanba" => $"{dushanba}",
+    "seshanba" => $"{seshanba}",
+    "chorshanba" => $"{chorshanba}",
+    "payshanba" => $"{payshanba}",
+    "juma" => $"{juma}",
+    "shanba" => $"{shanba}",
+    "yakshanba" => $"{yakshanba}",
+         _ => "Operation Not Found!"
+};
 
-        System.Console.WriteLine(message);
+System.Console.WriteLine(result);
